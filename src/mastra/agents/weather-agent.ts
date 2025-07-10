8efa347f80a3d15a0f400bc923ce68b84d626864
yoshiko <yoshiko@knowledgework.com>
2025-07-10 23:03:07 +0900

    永続化

--- src/mastra/agents/weather-agent.ts
+++ src/mastra/agents/weather-agent.ts
@@ -1,7 +1,6 @@
 import { openai } from "@ai-sdk/openai";
 import { Agent } from "@mastra/core/agent";
 import { Memory } from "@mastra/memory";
-import { LibSQLStore } from "@mastra/libsql";
 import { weatherTool } from "../tools/weather-tool";
 
 export const weatherAgent = new Agent({
@@ -22,9 +21,5 @@ export const weatherAgent = new Agent({
 `,
   model: openai("gpt-4o-mini"),
   tools: { weatherTool },
-  memory: new Memory({
-    storage: new LibSQLStore({
-      url: "file:../mastra.db", // path is relative to the .mastra/output directory
-    }),
-  }),
+  memory: new Memory(),
 });
--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,5 +1,4 @@
 import { Mastra } from "@mastra/core/mastra";
-import { PinoLogger } from "@mastra/loggers";
 import { LibSQLStore } from "@mastra/libsql";
 
 import { weatherAgent } from "./agents/weather-agent";
@@ -7,11 +6,7 @@ import { weatherAgent } from "./agents/weather-agent";
 export const mastra = new Mastra({
   agents: { weatherAgent },
   storage: new LibSQLStore({
-    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:",
-  }),
-  logger: new PinoLogger({
-    name: "Mastra",
-    level: "info",
+    // workaround: https://github.com/mastra-ai/mastra/issues/2806
+    url: "file:../../mastra.db",
   }),
 });
